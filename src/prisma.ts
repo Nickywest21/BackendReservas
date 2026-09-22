7754b5281580a3dd66ced9994bbd67fe027d4ff5
Juan Pablo <juanpaestra8@gmail.com>
2026-09-22 01:09:58 -0600

    Implementacion de la base de datos

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "./env.js";
 
-export const prisma = new PrismaClient();
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+
+export const prisma = new PrismaClient({ adapter });
